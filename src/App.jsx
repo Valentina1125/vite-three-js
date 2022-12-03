18a404083486b9ac523e18bb100159585e3a0d32
Valentina Uribe <valentinauribe11257@gmail.com>
2022-12-03 14:01:31 -0500

    version estable

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import load from "./assets/load";
+import  objectURLs  from "./assets/constants";
+
+function App() {
+  useEffect(() => load(objectURLs.brazo), []);
+  return (
+    <div className="App">
+        <canvas id="canvas"></canvas>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/constants.js
@@ -0,0 +1,18 @@
+const objectURLs = {
+  brazo: {
+    textureURL: "brazo/brazo.jpg",
+    mtlURL: "brazo/brazo.mtl",
+    objURL: "brazo/brazo.obj",
+  },
+  craneo: {
+    textureURL: ["craneo/craneoinf01.jpg","craneo/craneoinf02.jpg"],
+    mtlURL: "craneo/craneoinf.mtl",
+    objURL: "craneo/craneoinf.obj",
+  },
+  orofaringe: {
+    textureURL: "orofaringe/Orofaringe01.jpg",
+    mtlURL: "orofaringe/Orofaringe.mtl",
+    objURL: "orofaringe/Orofaringe.obj",
+  },
+};
+export default objectURLs;
